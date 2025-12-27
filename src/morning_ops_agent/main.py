9b8b0e2fedcfea48e7bb2cc9ab3a64b16f350eb5
Rishabh Sinha <rishabhsinha220@gmail.com>
2025-12-27 01:59:21 -0500

    Integrating with notion to share daily updates

--- src/morning_ops_agent/main.py
+++ src/morning_ops_agent/main.py
@@ -1,7 +1,9 @@
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
 from agents import agent
+from notion_client import Client
 
 
 
@@ -11,9 +13,55 @@ def run(city):
     _agent = agent.get_agent()
 
     response = _agent.invoke({"input": prompt})
-    print(response["messages"][-1].content)
+    return response["messages"][-1].content
 
 
 
 if __name__ == "__main__":
-    run("Quincy, MA")
+    response = run("Quincy, MA")
+    print("Adding to notion: ", response)
+
+    notion = Client(auth=os.getenv("NOTION_TOKEN"))
+    page_id = os.getenv("NOTION_PAGE_ID")
+
+    # Append a divider
+    notion.blocks.children.append(
+        block_id=page_id,
+        children=[
+            {
+                "object": "block",
+                "type": "divider",
+                "divider": {}
+            }
+        ]
+    )
+    
+    notion.blocks.children.append(
+        block_id=page_id,
+        children=[
+            {
+                "object": "block",
+                "type": "paragraph",
+                "paragraph": {
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": {"content": response}
+                        }
+                    ]
+                },
+            }
+        ],
+    )
+
+    # Append a divider
+    notion.blocks.children.append(
+        block_id=page_id,
+        children=[
+            {
+                "object": "block",
+                "type": "divider",
+                "divider": {}
+            }
+        ]
+    )
